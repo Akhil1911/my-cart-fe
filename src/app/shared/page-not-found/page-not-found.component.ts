8cda617d4bd7d5a8411c95be5acd36cb45051a21
Mrugesh Dixit <mrugesh.dixit@irisbusiness.com>
2024-11-23 21:59:26 +0530

    fix: 404 page add home page route

--- src/app/shared/page-not-found/page-not-found.component.ts
+++ src/app/shared/page-not-found/page-not-found.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { Store } from '../../Store/Store';
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
   imports: [],
   templateUrl: './page-not-found.component.html',
-  styleUrl: './page-not-found.component.css'
+  styleUrl: './page-not-found.component.css',
 })
 export class PageNotFoundComponent {
+  constructor(private router: Router, private store: Store) {}
 
+  handleRedirect() {
+    const hasAdminRole = this.store.activeUser() === 'admin';
+    this.router.navigate(hasAdminRole ? ['/admin', 'dashboard'] : ['/user']);
+  }
 }
